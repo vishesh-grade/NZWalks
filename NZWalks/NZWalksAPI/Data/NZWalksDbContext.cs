3719bc52180dad105563df3b9c1db235c91cb446
vishesh-grade <vishesh.grade18@gmail.com>
2022-11-26 23:22:16 +0530

    Section 2

---
+++ NZWalks/NZWalksAPI/Data/NZWalksDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NZWalksAPI.Models.Domain;
+
+namespace NZWalksAPI.Data
+{
+    public class NZWalksDbContext : DbContext
+    {
+        public NZWalksDbContext(DbContextOptions<NZWalksDbContext> options):base(options)
+        {
+
+        }
+        public DbSet<Region> Regions { get; set; }
+        public DbSet<Walk> Walks { get; set; }
+        public DbSet<WalkDifficulty> WalkDifficulty { get; set; }
+
+    }
+}
---
+++ NZWalks/NZWalksAPI/Models/Domain/Region.cs
@@ -0,0 +1,19 @@
+﻿namespace NZWalksAPI.Models.Domain
+{
+    public class Region
+    {
+        public Guid id { get; set; }
+        public String Code { get; set; }
+        public String Name { get; set; }
+        public double Area { get; set; }
+        public double Lat { get; set; }
+        public double Long { get; set; }
+        public long Population { get; set; }
+
+        //Navigation property
+
+        public IEnumerable<Walk> Walks { get; set; }
+
+
+    }
+}
